bd49c0c5a8fb5cbecfdae9b59a19712a345d510a
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-03-28 19:22:16 +0300

    add main.go

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/dronbrigadir/tech-mail-db-forum/pkg/server"
+	"log"
+)
+
+func main() {
+	s := server.NewServer("5000")
+	s.InitRoutes()
+
+	log.Println("starting server at :5000")
+	if err := s.Run(); err != nil {
+		log.Fatal(err, "can't run server")
+	}
+}
