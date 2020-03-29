af3676be7e4b9690f260cf0e34707a4d916bb9de
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-03-30 02:13:50 +0300

    rename swagger -> swaggerCntrl

--- internal/controllers/swaggerCntrl.go
+++ internal/controllers/swaggerCntrl.go
@@ -8,6 +8,6 @@ func SwaggerUI() http.Handler {
 	return http.StripPrefix("/swaggerui/", http.FileServer(http.Dir("./third-party/swagger-ui")))
 }
 
-func SwaggerApi (w http.ResponseWriter, r *http.Request) {
+func SwaggerApi(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./api/swagger.yml")
 }
