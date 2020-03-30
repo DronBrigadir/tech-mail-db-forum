5bc827207f711375bcec9d8c998f3d37c763e61e
Dronbrigadir <zubkov.andrey.99@mail.ru>
2020-03-30 23:09:08 +0300

    server: add routes

--- internal/server/server.go
+++ internal/server/server.go
@@ -21,6 +21,28 @@ func NewServer(port string) *Server {
 func (s *Server) InitRoutes() {
 	s.router.PathPrefix("/swaggerui/").Handler(controllers.SwaggerUI())
 	s.router.HandleFunc("/api/swagger.yml", controllers.SwaggerApi).Methods(http.MethodGet)
+
+	s.router.HandleFunc("/api/forum/create", controllers.ForumCreate).Methods(http.MethodPost)
+	s.router.HandleFunc("/api/forum/{slug}/details", controllers.ForumGet).Methods(http.MethodGet)
+	s.router.HandleFunc("/api/forum/{slug}/create", controllers.CreateForumThread).Methods(http.MethodPost)
+	s.router.HandleFunc("/api/forum/{slug}/threads", controllers.GetForumThreads).Methods(http.MethodGet)
+	s.router.HandleFunc("/api/forum/{slug}/users", controllers.GetForumUsers).Methods(http.MethodGet)
+
+	s.router.HandleFunc("/api/thread/{slug_or_id}/create", controllers.CreateThreadPost).Methods(http.MethodPost)
+	s.router.HandleFunc("/api/thread/{slug_or_id}/details", controllers.GetThread).Methods(http.MethodGet)
+	s.router.HandleFunc("/api/thread/{slug_or_id}/details", controllers.UpdateThread).Methods(http.MethodPost)
+	s.router.HandleFunc("/api/thread/{slug_or_id}/posts", controllers.GetThreadPosts).Methods(http.MethodGet)
+	s.router.HandleFunc("/api/thread/{slug_or_id}/vote", controllers.VoteThread).Methods(http.MethodPost)
+
+	s.router.HandleFunc("/api/user/{nickname}/create", controllers.UserCreate).Methods(http.MethodPost)
+	s.router.HandleFunc("/api/user/{nickname}/profile", controllers.GetUser).Methods(http.MethodGet)
+	s.router.HandleFunc("/api/user/{nickname}/profile", controllers.UpdateUser).Methods(http.MethodPost)
+
+	s.router.HandleFunc("/api/service/clear", controllers.ClearDB).Methods(http.MethodPost)
+	s.router.HandleFunc("/api/service/status", controllers.StatusDB).Methods(http.MethodGet)
+
+	s.router.HandleFunc("/api/post/{id}/details", controllers.GetPostDetails).Methods(http.MethodGet)
+	s.router.HandleFunc("/api/post/{id}/details", controllers.UpdatePost).Methods(http.MethodPost)
 }
 
 func (s *Server) Run() error {
